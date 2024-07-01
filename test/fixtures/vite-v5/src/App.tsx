39f2c215012af62409dc3cbd36529a3f1ae8cb3a
Jan Biasi <janbiasi@users.noreply.github.com>
2024-07-01 10:49:28 +0200

    fix: register imported components as dependencies, resolves #86

--- test/fixtures/vite-v5/src/App.tsx
+++ test/fixtures/vite-v5/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ComponentWithDeps } from './ComponentWithDeps'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <ComponentWithDeps />
     </>
   )
 }
--- test/vite-v5.test.ts
+++ test/vite-v5.test.ts
@@ -14,7 +14,14 @@ test("it should generate the JSON SBOM which matches the JSON schema spec versio
     expect(helpers.isBomValidAccordingToSchema("v1.6", bom)).toBeTruthy();
 });
 
-describe.concurrent("JSON", () => {
+test("it should generate the well-known file correctly", async () => {
+    const bom = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
+    const wellKnownBom = await helpers.getCompiledFileJSONContent(".well-known/sbom");
+
+    expect(bom).toStrictEqual(wellKnownBom);
+});
+
+describe.concurrent("Metadata", () => {
     test("it should generate a valid urn serial", async () => {
         const bom = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
 
@@ -35,47 +42,13 @@ describe.concurrent("JSON", () => {
         });
     });
 
-    test("it should autodetect the root application correctly", async () => {
+    test("it should autodetect the root application component correctly", async () => {
         const { metadata } = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
 
         expect(metadata.component).toBeDefined();
         expect(metadata.component.type).toEqual("application");
         expect(metadata.component.name).toEqual("vite-v5");
         expect(metadata.component.group).toEqual("@fixtures");
-    });
-
-    test("it should detect production dependencies correctly", async () => {
-        const { components } = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
-        const dependencyNames = components.map((component) => component.name);
-
-        expect(dependencyNames).toContain("react");
-        expect(dependencyNames).toContain("react-dom");
-    });
-
-    // test case to prevent occured issue:
-    // https://github.com/janbiasi/rollup-plugin-sbom/issues/10
-    test("it should register dependencies only once (issue #10)", async () => {
-        const { components } = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
-        const dependencyNames = components.map((component) => component.name);
-        const uniqueDependencyNames = dependencyNames.filter((name, index) => dependencyNames.indexOf(name) === index);
-
-        expect(dependencyNames).toEqual(uniqueDependencyNames);
-    });
-
-    test("it should set the supplier correctly when configured", async () => {
-        const { metadata } = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
-
-        expect(metadata.supplier).toEqual({
-            name: "Supplier Example Inc",
-            url: ["https://example.com"],
-            contact: [
-                {
-                    name: "Contact Name",
-                    email: "example@example.com",
-                    phone: "111-222-4444",
-                },
-            ],
-        });
     });
 
     test("it should support setting custom properties", async () => {
@@ -95,5 +68,75 @@ describe.concurrent("JSON", () => {
                 value: "duplicate-value-2",
             },
         ]);
+    });
+
+    test("it should set the supplier correctly when configured (issue #12)", async () => {
+        const { metadata } = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
+
+        expect(metadata.supplier).toEqual({
+            name: "Supplier Example Inc",
+            url: ["https://example.com"],
+            contact: [
+                {
+                    name: "Contact Name",
+                    email: "example@example.com",
+                    phone: "111-222-4444",
+                },
+            ],
+        });
+    });
+});
+
+describe("Components", () => {
+    test("it should detect production dependencies as components correctly", async () => {
+        const { components } = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
+        const componentNames = components.map((component) =>
+            // recompute full NPM name if applicable
+            component.group ? `${component.group}/${component.name}` : component.name,
+        );
+
+        // direct dependencides
+        expect(componentNames).toContain("react");
+        expect(componentNames).toContain("react-dom");
+        expect(componentNames).toContain("@mui/base");
+        expect(componentNames).toContain("date-fns");
+        // transitive dependencies
+        expect(componentNames).toContain("@mui/utils");
+        expect(componentNames).toContain("prop-types");
+        expect(componentNames).toContain("clsx");
+        expect(componentNames).toContain("react-is");
+        // injected runtime dependencies
+        expect(componentNames).toContain("@babel/runtime");
+        expect(componentNames).toContain("scheduler");
+    });
+
+    // test case to prevent occured issue:
+    // https://github.com/janbiasi/rollup-plugin-sbom/issues/10
+    test("it should register components only once (issue #10)", async () => {
+        const { components } = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
+        const dependencyNames = components.map((component) => component.name);
+        const uniqueDependencyNames = dependencyNames.filter((name, index) => dependencyNames.indexOf(name) === index);
+
+        expect(dependencyNames).toEqual(uniqueDependencyNames);
+    });
+});
+
+describe("Dependencies", () => {
+    test("it should not list dependencies multiple times (#10)", async () => {
+        const { dependencies } = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
+        const dependencyBomRefs = dependencies.map((entry) => entry.ref);
+
+        for (const [index, ref] of dependencyBomRefs.entries()) {
+            expect(dependencyBomRefs.indexOf(ref)).toEqual(index);
+        }
+    });
+
+    test("it should add the reigstered components as dependencies correctly (issue #86)", async () => {
+        const { components, dependencies } = await helpers.getCompiledFileJSONContent("plugin-outdir/filename.json");
+        const dependencyBomRefs = dependencies.map((entry) => entry.ref);
+
+        for (const component of components) {
+            expect(dependencyBomRefs).toContain(component["bom-ref"]);
+        }
     });
 });
