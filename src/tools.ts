06c0c5d11f6df65b75c5ad3d6afca85a06239393
Jan Biasi <janbiasi@users.noreply.github.com>
2025-05-14 00:42:03 +0200

    fix: use post-treeshaking module graph for generation

--- src/tools.ts
+++ src/tools.ts
@@ -1,23 +1,13 @@
 import { createRequire } from "node:module";
 
-import { type Builders, type Models, type Factories } from "@cyclonedx/cyclonedx-library";
+import { type Builders, type Models } from "@cyclonedx/cyclonedx-library";
 import { getCorrespondingPackageFromModuleId } from "./helpers";
 
 const require = createRequire(import.meta.url);
 
 const knownTools = ["rollup-plugin-sbom", "vite", "rollup"];
 
-export function registerPackageUrlOnComponent(
-    component: Models.Component | undefined,
-    factory: Factories.FromNodePackageJson.PackageUrlFactory,
-) {
-    if (component) {
-        component.purl = factory.makeFromComponent(component);
-        component.bomRef.value = component.purl?.toString();
-    }
-}
-
-export async function registerTools(bom: Models.Bom, builder: Builders.FromNodePackageJson.ToolBuilder) {
+export async function autoRegisterTools(bom: Models.Bom, builder: Builders.FromNodePackageJson.ToolBuilder) {
     async function registerTool(packageName: string) {
         try {
             const modulePath = require.resolve(packageName);
