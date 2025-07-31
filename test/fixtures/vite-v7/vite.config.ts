2a5c82292d3dae22bfa22b289f51d4d021f96cbb
Jan Biasi <janbiasi@users.noreply.github.com>
2025-07-31 10:25:53 +0200

    feat!: update cyclonedx library and move to latest lts
    BREAKING CHANGE: dropped support for end of life node.js versions, now requires >= 20.19.0

---
+++ test/fixtures/vite-v7/vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import sbom from 'rollup-plugin-sbom';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            logLevel: 'debug'
+        }
+    },
+  plugins: [react(), sbom({
+    autodetect: true,
+    generateSerial: true,
+    includeWellKnown: true,
+    outDir: "plugin-outdir",
+    outFilename: "filename",
+    outFormats: ["json", "xml"],
+    saveTimestamp: true,
+    collectLicenseEvidence: true,
+    supplier: {
+        name: "Supplier Example Inc",
+        url: ["https://example.com"],
+        contact: [{
+            name: "Contact Name",
+            email: "example@example.com",
+            phone: "111-222-4444"
+        }]
+    },
+    properties: [{
+        name: "unique-key",
+        value: "unique-value"
+    }, {
+        name: "duplicate-key",
+        value: "duplicate-value-1"
+    }, {
+        name: "duplicate-key",
+        value: "duplicate-value-2"
+    }],
+  })],
+})
