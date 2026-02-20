a2e83475ab58b87ee1f67e3e8f3fd173e674ac68
Jan Biasi <janbiasi@users.noreply.github.com>
2026-02-20 13:10:49 +0100

    fix: improve resilience for missing dependency info, add module log info

--- src/index.ts
+++ src/index.ts
@@ -66,12 +66,19 @@ export default function rollupPluginSbom(userOptions?: RollupPluginSbomOptions):
     function processExternalModuleForBom(context: PluginContext, mod: ExternalModuleInfo) {
         const dependencyInfo = dependencyInfoRegistry.get(mod.modulePath);
         if (!dependencyInfo) {
-            context.warn(`Missing dependency info for module ${mod.modulePath} in registry, this should not happen.`);
+            context.info(`Could not resolve dependency info for module ID: ${mod.moduleId}`);
+            context.info(`${mod.moduleInfo.code}`);
+            context.warn({
+                message: `Missing dependency info for module ${mod.modulePath} in registry, this should not happen (ID: ${mod.moduleId})`,
+                meta: mod,
+            });
         }
 
-        const { pkg, licenseEvidence } = dependencyInfo;
+        const { pkg, licenseEvidence } = dependencyInfo || {};
         if (!pkg || !pkg.name || !pkg.version) {
-            context.warn(`Missing package data for module ${mod.modulePath} in registry, this should not happen.`);
+            context.warn(
+                `Missing package data for module ${mod.modulePath} in registry, this should not happen (ID: ${mod.moduleId})`,
+            );
             return;
         }
 
